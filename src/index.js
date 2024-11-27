12c228f8c467abaab276ca27623fea256f97eab6
DaniellaOhenewaaAsumadu <you@example.com>
2024-11-27 14:20:04 +0000

    added a few changes

---
+++ src/index.js
@@ -0,0 +1,4 @@
+let name=prompt("your name");
+let age=prompt("your age");
+let email=prompt("your email");
+alert(`Hi ${name}, you're ${age} years old  and well keep in touch via your email:${email}`);
